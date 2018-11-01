8c9739397934ed18914ff0a681d8447cb910eca6
Kim Jong Feel <DeveloperFEEL@gmail.com>
2018-11-01 19:24:40 +0900

    Add problem 15552, missing addtional problem during second week

---
+++ Problems/15552/Program/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace Problem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            int t = int.Parse(s1);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < t; i++)
+            {
+                string s2 = Console.ReadLine();
+                string[] s22 = s2.Split();
+                int a = int.Parse(s22[0]);
+                int b = int.Parse(s22[1]);
+
+                sb.Append(a + b + "\n");
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
