a78deff8f2423284b9d4ae61d17ca44b4307ebb0
Kim Jong Feel <DeveloperFEEL@gmail.com>
2018-11-17 14:33:13 +0900

    Add problem 10039

---
+++ Problems/10039/Program.test/UnitTest1.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using Problem;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace test
+{
+    public class CustomDataSourceAttribute : Attribute, ITestDataSource
+    {
+        public IEnumerable<object[]> GetData(MethodInfo methodInfo)
+        {
+            yield return new object[] { new int[] { 10, 65, 100, 30, 95 }, 68};
+        }
+
+        public string GetDisplayName(MethodInfo methodInfo, object[] data)
+        {
+            if (data != null)
+                return string.Format(CultureInfo.CurrentCulture, "Custom - {0} ({1})", methodInfo.Name, string.Join(",", data));
+
+            return null;
+        }
+    }
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataTestMethod]
+        [CustomDataSource]
+        public void TestMethod(int[] values, int expected)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            Trace.WriteLine($"{values} {expected}");
+            Assert.AreEqual(Program.GetSupplementaryAverage(values), expected);
+        }
+    }
+}
---
+++ Problems/10039/Program/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                string s1 = Console.ReadLine();
+                scores[i] = int.Parse(s1);
+            }
+            
+            Console.WriteLine((int)GetSupplementaryAverage(scores));
+        }
+
+        public static double GetSupplementaryAverage(int[] scores)
+        {
+            return scores.Select(score => score < 40 ? 40 : score).Average();
+        }
+    }
+}
